5c27f3f7fe406e1edcab132645f87cdb0c83af0c
a.monir <aslm.monir@paysky.io>
2023-02-08 13:21:42 +0200

    * initial commit

--- app/src/main/java/com/example/nfcdemoandroid/MainActivity.kt
+++ app/src/main/java/com/example/nfcdemoandroid/MainActivity.kt
@@ -1,22 +1,36 @@
 package com.example.nfcdemoandroid
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.FormatException
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.*
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.nfcdemoandroid.databinding.ActivityMainBinding
+import com.example.nfcdemoandroid.ui.NFCRead.TAG
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var nfcAdapter: NfcAdapter? = null
+    lateinit var mPendingIntent: PendingIntent
+    var tag: Tag? = null
 
+    var intentFiltersArray = arrayOf<IntentFilter>()
+    var techListsArray = arrayOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +39,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply {
+            try {
+                addDataType("*/*")    /* Handles all MIME based dispatches.
+                                 You should specify only the ones that you need. */
+            } catch (e: IntentFilter.MalformedMimeTypeException) {
+                throw RuntimeException("fail", e)
+            }
+        }
+
+        intentFiltersArray = arrayOf(ndef)
+
+        techListsArray = arrayOf<String>(
+            NfcA::class.java.name,
+            NfcB::class.java.name,
+            NfcF::class.java.name,
+            NfcV::class.java.name,
+            Ndef::class.java.name,
+            NdefFormatable::class.java.name,
+            MifareClassic::class.java.name,
+            MifareUltralight::class.java.name,
+            IsoDep::class.java.name,
+
+
+            )
+        mPendingIntent = PendingIntent.getActivity(
+            this, 0, Intent(
+                this,
+                javaClass
+            ).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_MUTABLE
+        )
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -37,15 +82,123 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (nfcAdapter==null){
+        if (nfcAdapter == null) {
             Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show()
         }
 
     }
-    fun getNFCAdapter()= nfcAdapter
+
+    override fun onResume() {
+        super.onResume()
+//        nfcAdapter?.enableForegroundDispatch(
+//            this, 0, intentFiltersArray,
+//            0
+//        )
+        enableNfcForegroundDispatch()
+        /**
+         * TAG dispatch system used :
+         * Action_NDEF_DISCOVERD ->> when NDEF MESSAGES are obtained and successful of
+         * mapping to MimeType/URI with payload data
+         * Action_TECH_DISCOVERED ->>  when NDEF MEssages fails to be mapped to Mime Type /URI
+         * Action_TAG_DISCOVERD ->> when both above actions failed .
+         */
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == intent?.action ||
+            NfcAdapter.ACTION_TAG_DISCOVERED == intent?.action
+        ) {
+            processIntent(intent)
+        }
+
+
+    }
+
+
+    private fun enableNfcForegroundDispatch() {
+        try {
+            val intent = Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            val nfcPendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            getNFCAdapter()?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+        } catch (ex: IllegalStateException) {
+            Log.e("error", "Error enabling NFC foreground dispatch", ex)
+        }
+    }
+
+    private fun processIntent(intent: Intent) {
+        /**
+         * for records found in messages we have : 1- TNF (Type Name Format ) as : TNF_WELL_KNOWN ,TNF_UKNOWN
+         * 2- payload (data sent )
+         * 3- Type
+         */
+        val rawmsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+        val msgs = mutableListOf<NdefMessage>()
+        if (rawmsgs?.isNotEmpty() == true) {
+            rawmsgs.forEachIndexed { index, parcelable ->
+                msgs.add(rawmsgs[index] as NdefMessage)
+                msgs[0].records[0].payload
+            }
+        }
+        Log.e("NDEF Messages intent", msgs.toString())
+        Toast.makeText(this, msgs.toString(), Toast.LENGTH_LONG).show()
+//        _binding?.textHome?.text = msgs.toString()
+
+        val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        Log.e("tag intent", tag?.id.toString())
+
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+
+    fun getNFCAdapter() = nfcAdapter
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        setIntent(intent)
+
+        var tags = intent?.getParcelableArrayExtra(NfcAdapter.EXTRA_TAG)
+        Log.d(TAG, "onNewIntent: " + intent!!.action)
+
+        if (tag != null) {
+            val ndef = Ndef.get(tag)
+            Toast.makeText(
+                applicationContext, "${ndef.tag}",
+                Toast.LENGTH_SHORT
+            ).show()
+            readFromNFC(ndef)
+        } else {
+            Toast.makeText(
+                applicationContext, "Problem reading NFC tag!\nPlease try again.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        //setIntent(intent)
+    }
+
+    fun getNFCtag() = tag
+
+    private fun readFromNFC(ndef: Ndef) {
+        try {
+            ndef.connect()
+            val ndefMessage = ndef.ndefMessage
+            if (ndefMessage != null) {
+                val records = ndefMessage.records
+
+                //records will produce an array of strings stored on the tag
+                //iterate through them as handle as required.
+                Toast.makeText(
+                    applicationContext, "Record $records",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            ndef.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: FormatException) {
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/example/nfcdemoandroid/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/nfcdemoandroid/ui/dashboard/DashboardFragment.kt
@@ -1,8 +1,14 @@
 package com.example.nfcdemoandroid.ui.dashboard
 
+import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
+import android.nfc.NdefRecord.TNF_MIME_MEDIA
 import android.nfc.NdefRecord.createMime
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.MifareUltralight
+import android.nfc.tech.NfcA
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +21,7 @@ import com.example.nfcdemoandroid.databinding.FragmentDashboardBinding
 import java.nio.charset.Charset
 import java.util.*
 
+
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
@@ -22,6 +29,7 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    lateinit var tag: Tag
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,18 +46,22 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
         _binding?.btnSend?.setOnClickListener {
             val text = _binding?.textDashboard?.text
 
+
+            writeTag((requireActivity() as MainActivity).getNFCtag(), text.toString())
             /**
              * simple text/plain NDEF Message
              */
-            (requireActivity() as MainActivity).getNFCAdapter()?.setNdefPushMessage(createTextRecord(text.toString()), requireActivity())
+            //     (requireActivity() as MainActivity).getNFCAdapter()?.setNdefPushMessage(createTextRecord(text.toString()), requireActivity())
             /**
              * simple beam NDEF Message
              * - need to add same mimeType in Manifest as well
              */
-         //   (requireActivity() as MainActivity).getNFCAdapter()?.setNdefPushMessage(createNdefBeamMessage(),requireActivity())
+            //   (requireActivity() as MainActivity).getNFCAdapter()?.setNdefPushMessage(createNdefBeamMessage(),requireActivity())
 
         }
         return root
@@ -60,39 +72,58 @@ class DashboardFragment : Fragment() {
         _binding = null
     }
 
-    fun createTextRecord(
-        payload: String,
-        locale: Locale = Locale.getDefault(),
-        encodeInUtf8: Boolean = true
-    ): NdefMessage {
-        val langBytes = locale.language.toByteArray(Charset.forName("US-ASCII"))
-        val utfEncoding = if (encodeInUtf8) Charset.forName("UTF-8") else Charset.forName("UTF-16")
-        val textBytes = payload.toByteArray(utfEncoding)
-        val utfBit: Int = if (encodeInUtf8) 0 else 1 shl 7
-        val status = (utfBit + langBytes.size).toChar()
-        val data = ByteArray(1 + langBytes.size + textBytes.size)
-        data[0] = status.toByte()
-        System.arraycopy(langBytes, 0, data, 1, langBytes.size)
-        System.arraycopy(textBytes, 0, data, 1 + langBytes.size, textBytes.size)
-        return NdefMessage(
-            arrayOf(
-                NdefRecord(
-                    NdefRecord.TNF_WELL_KNOWN,
-                    NdefRecord.RTD_TEXT,
-                    ByteArray(0),
-                    data
-                )
-            )
-        )
+
+    fun writeTag(tag: Tag?, tagText: String) {
+//        val typeBytes = mimeType.toByteArray()
+//        val payload = tagData.toByteArray()
+//        val r1 = NdefRecord(TNF_MIME_MEDIA, typeBytes, null, payload)
+//        val r2 = NdefRecord.createApplicationRecord(context?.packageName)
+//        var ndefMessage = NdefMessage(arrayOf(r1, r2))
+
+        NfcA.get(tag)?.use { ultralight ->
+            ultralight.connect()
+            Charset.forName("US-ASCII").also { usAscii ->
+//                ultralight.writePage(4, "abcd".toByteArray(usAscii))
+//                ultralight.writePage(5, "efgh".toByteArray(usAscii))
+//                ultralight.writePage(6, "ijkl".toByteArray(usAscii))
+//                ultralight.writePage(7, "mnop".toByteArray(usAscii))
+            }
+        }
     }
 
-    fun createNdefBeamMessage(): NdefMessage {
-        val text = "Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis()
-        return NdefMessage(
-            arrayOf(
-                createMime("application/vnd.com.example.android.beam", text.toByteArray())
-            )
-        )
-    }
+//    fun createTextRecord(
+//        payload: String,
+//        locale: Locale = Locale.getDefault(),
+//        encodeInUtf8: Boolean = true
+//    ): NdefMessage {
+//        val langBytes = locale.language.toByteArray(Charset.forName("US-ASCII"))
+//        val utfEncoding = if (encodeInUtf8) Charset.forName("UTF-8") else Charset.forName("UTF-16")
+//        val textBytes = payload.toByteArray(utfEncoding)
+//        val utfBit: Int = if (encodeInUtf8) 0 else 1 shl 7
+//        val status = (utfBit + langBytes.size).toChar()
+//        val data = ByteArray(1 + langBytes.size + textBytes.size)
+//        data[0] = status.toByte()
+//        System.arraycopy(langBytes, 0, data, 1, langBytes.size)
+//        System.arraycopy(textBytes, 0, data, 1 + langBytes.size, textBytes.size)
+//        return NdefMessage(
+//            arrayOf(
+//                NdefRecord(
+//                    NdefRecord.TNF_WELL_KNOWN,
+//                    NdefRecord.RTD_TEXT,
+//                    ByteArray(0),
+//                    data
+//                )
+//            )
+//        )
+//    }
+
+//    fun createNdefBeamMessage(): NdefMessage {
+//        val text = "Beam me up, Android!\n\n" +
+//                "Beam Time: " + System.currentTimeMillis()
+//        return NdefMessage(
+//            arrayOf(
+//                createMime("application/vnd.com.example.android.beam", text.toByteArray())
+//            )
+//        )
+//    }
 }
