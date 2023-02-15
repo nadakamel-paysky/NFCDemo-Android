df35e9279d979152aecc2156605845b3f5b499a6
a.monir <aslm.monir@paysky.io>
2023-02-15 12:35:43 +0200

    added demo giph

--- app/src/main/java/com/example/nfcdemoandroid/utils/HceService.kt
+++ app/src/main/java/com/example/nfcdemoandroid/utils/HceService.kt
@@ -4,7 +4,7 @@ import android.nfc.cardemulation.HostApduService
 import android.os.Bundle
 import android.util.Log
 
-class HostCardEmulatorService: HostApduService() {
+class HostCardEmulatorService : HostApduService() {
     companion object {
         val TAG = "Host Card Emulator"
         var STATUS_SUCCESS = ""
@@ -16,12 +16,15 @@ class HostCardEmulatorService: HostApduService() {
         val DEFAULT_CLA = "00"
         val MIN_APDU_LENGTH = 12
     }
+
     override fun onDeactivated(reason: Int) {
         Log.d(TAG, "Deactivated: " + reason)
     }
 
-    override fun processCommandApdu(commandApdu: ByteArray?,
-                                    extras: Bundle?): ByteArray {
+    override fun processCommandApdu(
+        commandApdu: ByteArray?,
+        extras: Bundle?
+    ): ByteArray {
         if (commandApdu == null) {
             return KotlinUtils.hexStringToByteArray(STATUS_FAILED)
         }
@@ -39,9 +42,9 @@ class HostCardEmulatorService: HostApduService() {
             return KotlinUtils.hexStringToByteArray(INS_NOT_SUPPORTED)
         }
 
-        if (hexCommandApdu.substring(10, 24) == AID)  {
+        if (hexCommandApdu.substring(10, 24) == AID) {
             return STATUS_SUCCESS.toByteArray()
-          //  return KotlinUtils.hexStringToByteArray(STATUS_SUCCESS)
+            //  return KotlinUtils.hexStringToByteArray(STATUS_SUCCESS)
         } else {
             return KotlinUtils.hexStringToByteArray(STATUS_FAILED)
         }
